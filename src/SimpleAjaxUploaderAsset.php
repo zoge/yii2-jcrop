9aa571ba609a051dd854c707d861b70d1a5f3646
Ehsan Rezaei <mr.rezaee121@gmail.com>
2019-09-06 14:07:24 +0430

    Update SimpleAjaxUploaderAsset.php

--- src/SimpleAjaxUploaderAsset.php
+++ src/SimpleAjaxUploaderAsset.php
@@ -3,7 +3,7 @@ namespace developit\jcrop;
 use yii\web\AssetBundle;
 class SimpleAjaxUploaderAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/bower/simple-ajax-uploader/';
+    public $sourcePath = '@vendor/bower-asset/simple-ajax-uploader/';
     public $js = [
         'SimpleAjaxUploader.min.js'
     ];
