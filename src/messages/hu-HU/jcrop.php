e947d7dc43f920099546071ecad43718ce6b3e83
zoge <mail@e-dog.hu>
2020-01-24 19:38:40 +0100

    Create jcrop.php

---
+++ src/messages/hu-HU/jcrop.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'Drag Photo' => 'húzz ide egy képet',
+    'Select Photo' => 'válasz képet a számítógépedről',
+    'Or' => '- vagy -',
+    'Crop Photo' => 'Fotó körbe vágása',
+    'Select Another Photo' => 'Válasz másik képet',
+    'File Size Error' => 'Túl nagy fájl. Mérete {size} MB.',
+    'File Extension Error' => 'Csak a következő kiterjesztésű fájlok használhatóak: {formats}.',
+    'Missing Attribute' => 'Tulajdonság "{attribute}" kitöltése kötelező',
+    'Only POST Request' => 'Csak a POST típusú kérés engedélyezett',
+    'Can Not Upload File' => 'Feltöltés nem müködik',
+];
